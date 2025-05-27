013ecae7869d78c8632cd8a27aaee3ceade3ce0c
Alain Dias <alaindias@MacBook-Pro-de-Alain.local>
2025-05-27 12:36:55 +0200

    sprint_2

--- Sprint_1_MySQL/nivel_2/exercicio_1.sql
+++
@@ -1,8 +0,0 @@
--- Identifica los cinco días que generaron la mayor cantidad de ingresos por ventas
-SELECT DATE(timestamp) AS fecha_transaccion,
-		SUM(amount) AS ventas_totales
-FROM transaction
-WHERE declined = 0
-GROUP BY DATE(timestamp)
-ORDER BY ventas_totales DESC
-LIMIT 5;
---
+++ Sprint_2_MySQL/nivel_2/exercicio_3.sql
@@ -0,0 +1,17 @@
+-- En tu empresa, se plantea un nuevo proyecto para lanzar algunas campañas publicitarias para hacer competencia a la compañía 'Non Institute'. Para ello, te piden la lista de todas las transacciones realizadas por empresas que están situadas en el mismo país que esta compañía.
+-- Solución con JOIN y subconsulta
+SELECT t.*
+FROM transaction t
+JOIN company c ON t.company_id = c.id
+WHERE c.country = (SELECT country
+					FROM company
+                    WHERE company_name = 'Non Institute');
+                    
+-- Solución solo con subconsultas
+SELECT t.*
+FROM transaction t
+WHERE t.company_id IN (SELECT id
+						FROM company
+                        WHERE country = (SELECT country
+										FROM company
+                                        WHERE company_name ='Non Institute'));
